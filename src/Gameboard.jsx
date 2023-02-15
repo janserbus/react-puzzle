6a7eedc23d0b3fd8432aaa49a47cd853d125c1e8
Jan Serbus <jserbus@seznam.cz>
2023-02-15 15:42:48 +0100

    json update

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -16,15 +16,27 @@ function Gameboard() {
       console.log(data);
       setColumns(data.columns);
       setRows(data.rows);
+      
+      for(const piece of data.puzzle){
+        piece.curentX = piece.originX;
+        piece.curentY = piece.originY;
+      }
+      
+      for(const piece of data.puzzle){
+        let randpiece = data.puzzle[Math.floor(Math.random() * data.puzzle.length)];
+        [piece.curentX, randpiece.curentX] = [randpiece.curentX, piece.curentX];
+        [piece.curentY, randpiece.curentY] = [randpiece.curentY, piece.curentY];
+      }
+      
       setPuzzle(data.puzzle);
     });         
   });
 
 
-  const images = puzzle.map((image, index) => <Image />)
+  const images = puzzle.map((image, index) => <Image state={image} />)
 
   return (
-    <section className="gameboard">
+    <section className="gameboard" style={{gridTemplateColumns: "repeat(" + columns +", 1fr)", gridTemplateRows: "repeat(" + rows + ", 1fr)"}}>
       {images}
     </section>
   );
--- src/Image.jsx
+++ src/Image.jsx
@@ -1,6 +1,11 @@
-function Image(){
+import {Path} from "./Gameboard"
+
+function Image({state}){
+
+    console.log(state);
     return(
-        <img />
+
+        <img src={Path + state.url} style={{gridRow: state.curentY, gridColumn: state.curentX}}/>
     );
 };
 export default Image;
